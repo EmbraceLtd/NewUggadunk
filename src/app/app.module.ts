92111ba314cf053cbda504cef5c792e6c665d441
Tommy Sjöblom <nemrilius@hotmail.com>
2021-01-29 23:01:23 +0100

    Rensat demomaterial. Uggadunks bugg.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,7 @@ import { MenuModule, PanelModule, ChartModule, InputTextModule, ButtonModule, In
 
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { StatisticComponent } from './statistic/statistic.component';
-import { TimesheetComponent } from './timesheet/timesheet.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SettingsComponent } from './settings/settings.component';
-
-import { AlltimesComponent } from './alltimes/alltimes.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { FielderrorsComponent } from './fielderrors/fielderrors.component';
 import { MooseComponent } from './moose/moose.component';
 import { HitlistComponent } from './hitlist/hitlist.component';
 import { UggabuggComponent } from './uggabugg/uggabugg.component';
@@ -38,14 +29,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    StatisticComponent,
-    TimesheetComponent,
-    ProjectsComponent,
-    AlltimesComponent,
-    ProfileComponent,
-    SettingsComponent,
-    FielderrorsComponent,
     MooseComponent,
     HitlistComponent,
     UggabuggComponent,
--- src/app/uggabugg/uggabugg.component.ts
+++ src/app/uggabugg/uggabugg.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'at-uggabugg',
   templateUrl: './uggabugg.component.html',
@@ -7,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UggabuggComponent implements OnInit {
 
-  constructor() { }
+  displayBasic = false;
 
   ngOnInit() {
   }
 
+  showBasicDialog() {
+    this.displayBasic = true;
+  }
 }
